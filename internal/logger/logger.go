68bdea3d2e79ebc8583a90d85ad94228c54c87ea
60050 <C:\Users\user\AppData\Roaming\The Bat!>
2025-12-10 09:16:46 +0300

    feat(logger): add logger interface

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,1 +1,25 @@
+// Package logger provides logging utilities
 package logger
+
+// Logger defines the logger contract
+type Logger interface {
+	// Info logs a message at Info levevl.
+	// Args must be provided as key-value pairs
+	Info(msg string, args ...any)
+
+	// Debug logs a message at Debug levevl.
+	// Args must be provided as key-value pairs
+	Debug(msg string, args ...any)
+
+	// Warn logs a message at Warn levevl.
+	// Args must be provided as key-value pairs
+	Warn(msg string, args ...any)
+
+	// Error logs a message at Error levevl.
+	// Args must be provided as key-value pairs
+	Error(msg string, args ...any)
+
+	// Return new logger with context(static key-value pairs)
+	// Args must be a provided as key-value pairs
+	With(msg string, args ...any) Logger
+}
