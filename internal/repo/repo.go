accbaa3af0a17e796f8418dc7f94d5ccbc3918c5
60050 <C:\Users\user\AppData\Roaming\The Bat!>
2025-12-10 09:24:53 +0300

    feat(repo): refactor interface

--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -1,6 +1,8 @@
 // Package repo provides an interface for work with the repository
 package repo
 
-type UserRepository interface {
-	Create(email, password string) error
-}
+type FileRepository interface{}
+
+type FolderRepository interface{}
+
+type UserRepository interface{}
